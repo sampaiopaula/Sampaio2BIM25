d7bd1ef3102a40e4c2449466db7587092cf61e0b
Paula Sampaio <paulasampaio30ti@gmail.com>
2025-04-25 12:09:47 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <h1>NOME DO BLOG</h1>
+          <div>
+            <h2>PESSOA </h2>
+            <p>tesxtoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</p>
+            <p>DD/MM/AAAA</p>
+            <p>00:00</p>
+          </div>
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
